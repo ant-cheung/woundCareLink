40afae9b587a5e1ce8ea214bdcbe0c3955002e5c
Touraj Vaziri <vtouraj@gmail.com>
2016-11-24 15:46:00 -0800

    The image for reply message should come from logged in user.

--- app/pages/message/sendMessage.component.ts
+++ app/pages/message/sendMessage.component.ts
@@ -2,19 +2,25 @@ import {Component, Input} from '@angular/core';
 import {Message} from '../models/message';
 import {ControlGroup, Control, Validators} from '@angular/common';
 import { Events } from 'ionic-angular';
+import { User } from '../user/user.component';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component(
     {
         selector: "SendMessageComponent-item",
         templateUrl: "build/pages/message/sendMessage.html",
+        providers: [AuthenticationService]
     }
 )
 
 export class SendMessageComponent {
     @Input('init') message: Message;
     private form: ControlGroup;
+    public loggedInUser: User;
 
-    constructor(public events: Events) { }
+    constructor(public events: Events, private authenticationService: AuthenticationService) {
+                this.loggedInUser = this.authenticationService.getCurrentUser();
+     }
 
     ngOnInit() {
         this.createForm();
